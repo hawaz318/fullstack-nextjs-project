37d4cf99861218c35721e84b3115dfaebcef7af4
melkamu ayalew <melkamuayal21@gmail.com>
2025-08-07 00:19:01 +0300

    commited

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -45,6 +45,7 @@ export async function GET(req: NextRequest) {
   }
 }
 
+
 export async function POST(req: NextRequest) {
   try {
     const token = req.headers.get('authorization')?.split(' ')[1];
